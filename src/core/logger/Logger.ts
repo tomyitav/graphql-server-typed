b39234cb1680728f111cd465fe383a5eb90d646d
Tom <tomyitav@gmail.com>
2019-01-13 21:31:44 +0200

    Seemed to work

--- src/core/logger/Logger.ts
+++ src/core/logger/Logger.ts
@@ -2,13 +2,12 @@ import * as fs from 'fs'
 import {Injectable} from 'injection-js'
 import * as winston from 'winston'
 import * as DailyRotate from 'winston-daily-rotate-file'
-import LoggerInstance = winston.LoggerInstance
 import {AbstractSetting} from '../config/AbstractSetting'
 import {AbstractLogger} from './AbstractLogger'
 
 @Injectable()
 export class Logger extends AbstractLogger {
-  private logger: LoggerInstance
+  private logger: winston.Logger
 
   constructor(private settings: AbstractSetting) {
     super()
@@ -29,18 +28,16 @@ export class Logger extends AbstractLogger {
   }
 
   private initializeLogger() {
-    this.logger = new winston.Logger({
+    this.logger = winston.createLogger({
       level: 'info',
       transports: [
-        new winston.transports.Console({
-          colorize: true
-        }),
+        new winston.transports.Console(),
         new DailyRotate({
           filename: this.settings.config.log.filename,
           dirname: this.settings.config.log.filedir,
-          maxsize: 20971520, // 20MB
+          maxSize: 20971520, // 20MB
           maxFiles: 25,
-          datePattern: '.dd-MM-yyyy'
+          datePattern: 'DD-MM-YYYY'
         })
       ]
     })
